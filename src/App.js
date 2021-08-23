3efa9596f165703df5960e823999d6ce3d99d147
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-23 15:08:14 +0800

    [ST102][Robert] feat: Added UserLogin js file with respective Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './features/chingqueue/styles/App.css'
+import UserLogin from './features/chingqueue/components/UserLogin'
+import {Route, BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+       <BrowserRouter>
+       <Switch>
+          <Route exact path="/login" component={UserLogin}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/features/chingqueue/components/UserLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserLogin() {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default UserLogin
