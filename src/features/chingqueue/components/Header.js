b5fd5ea3c4482d98d5f25a8c2d8ee766353d50a6
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-23 21:49:00 +0800

    [ST201_List_of_Movies][Kyle] feat: Added Signin in UI and Menu for account

--- src/features/chingqueue/components/Header.js
+++ src/features/chingqueue/components/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import {Layout, Input, Image } from 'antd';
+import {Layout, Input, Image, Modal, Button } from 'antd';
 import { AudioOutlined } from '@ant-design/icons';
 import '../styles/header.css'
+import React, { useState } from 'react';
+import Settings from './Settings';
 
 
 const Header = () => {
+  
   const { Header } = Layout;
   const { Search } = Input;
   const onSearch = value => console.log(value);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const suffix = (
     <AudioOutlined
@@ -20,13 +36,33 @@ const Header = () => {
   
   return( 
     
+    <React.Fragment>
     <Header >
-      <div className="logo" />
+      
         <div className="location">
-        <img  className="responsive "src="https://i.imgur.com/bP82k1G.png" />
+        <img  className="responsive " alt="ChingQueue" src="https://i.imgur.com/bP82k1G.png" />
         <Search className="search" placeholder="input search text" onSearch={onSearch} enterButton />
-      </div>
+        
+        <span className="username">
+        Username text here
+        <Button type="primary" className="SignIn" onClick={showModal}>
+        Sign In 
+        </Button> 
+        <Settings className="Settings"/>
+        </span>
+
+      <>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      </>
+    </div>     
+     
     </Header>
+    
+    </React.Fragment>
     
     
     )
--- src/features/chingqueue/components/MovieCarousel.js
+++ src/features/chingqueue/components/MovieCarousel.js
@@ -1,5 +1,6 @@
 import { Carousel } from 'antd';
 import '../styles/movieCarousel.css'
+import React from 'react';
 
 
 function MovieCarousel(){
@@ -15,16 +16,16 @@ function MovieCarousel(){
     <div>
       <Carousel autoplay>
         <div>
-          <h3 style={contentStyle}><img ant="movie 1" className="movie1" src="https://i.imgur.com/zzELfqV.jpg%22/%3E"/></h3>
+          <h3 style={contentStyle}><img ant="movie 1" className="movie1" alt="Movie 1" src="https://i.imgur.com/zzELfqV.jpg%22/%3E"/></h3>
         </div>
         <div>
-          <h3 style={contentStyle}><img ant="movie 2"  className="movie2" src="https://blog.frame.io/wp-content/uploads/2020/01/parasite_header.jpg" /></h3>
+          <h3 style={contentStyle}><img ant="movie 2"  className="movie2" alt="Movie 2" src="https://blog.frame.io/wp-content/uploads/2020/01/parasite_header.jpg" /></h3>
         </div>
         <div>
-          <h3 style={contentStyle}><img ant="movie 3" className="movie3" src="https://i.imgur.com/TSMSr7W.jpg" /></h3>
+          <h3 style={contentStyle}><img ant="movie 3" className="movie3" alt="Movie 3" src="https://i.imgur.com/TSMSr7W.jpg" /></h3>
         </div>
         <div>
-          <h3 style={contentStyle}><img ant="movie 4" className="movie4" src="https://lwlies.com/wp-content/uploads/2016/11/the-wailing.jpg" /></h3>
+          <h3 style={contentStyle}><img ant="movie 4" className="movie4" alt="Movie 4" src="https://lwlies.com/wp-content/uploads/2016/11/the-wailing.jpg" /></h3>
         </div>
       </Carousel>
     </div>
