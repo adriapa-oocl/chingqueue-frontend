114ca521a60a42a68cf7c1ea78c60d03d5b67284
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-23 15:29:29 +0800

    [ST102][Robert] feat: Added barebones login form for UserLogin

--- src/features/chingqueue/components/UserLogin.js
+++ src/features/chingqueue/components/UserLogin.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
 
 function UserLogin() {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    
+      const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+
     return (
-        <div>
-            Test
+        <div className="login-form">
+            <Form name="loginForm"
+                labelCol={{span: 11,}}
+                wrapperCol={{span: 3,}}
+                initialValues={{remember: true,}}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+            <Form.Item label="Username" name="username"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item label="Password" name="password"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your password!',
+                },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked"
+                wrapperCol={{
+                offset: 7,
+                span: 10,
+                }}
+            >
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                offset: 8,
+                span: 9,
+                }}
+            >
+                <Button type="primary" htmlType="submit">
+                Submit
+                </Button>
+            </Form.Item>
+        </Form>
         </div>
     )
 }
