4b3b60329936a7c8ca73c6fd57c4339311ba5180
kudoyuto <yuto.kudo@oocl.com>
2021-08-23 23:28:29 +0800

    [ST201ListofMovies][Yuto&Kyle&Edward] refactor: redesign UI for now showing

---
+++ src/features/chingqueue/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Content} from 'antd';
+import MovieCarousel from './MovieCarousel'
+
+function Contents() {
+    return (
+        <Content>
+            <MovieCarousel/>
+        </Content>
+    )
+}
+
+export default Contents
