a368f89514c1dff7cc90a703f1259831aa99da42
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-24 16:39:20 +0800

    [ST103][Robert] feat: Added a new js for List of NowShowing and separated each movie as a component for looping purposes

--- src/features/chingqueue/components/NowShowing.js
+++ src/features/chingqueue/components/NowShowing.js
@@ -1,138 +1,27 @@
 
-import { Row, Col, Divider } from 'antd';
+import { Row, Col } from 'antd';
 import '../styles/nowShowing.css'
 
-function NowShowingCarousel(){
-   
-  
+function NowShowing(props){
     return(
       <div>
-        
-            <div>
-            <Divider orientation="left"></Divider>
-            <h2 className="nowShowing">Now Showing</h2>
- 
-            <Row gutter={[20, 48]} align="center">
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 1" className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/originals/05/f7/ea/05f7ea4755741c023f9e75e3df4b8212.jpg"/>
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 2"  className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/564x/c2/4c/d9/c24cd920faacaa8dbaa2cbbafdf83ac9.jpg" />
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 3" className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/564x/b3/42/d6/b342d651598fde2d9cfa8276c0a68df9.jpg" />
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 4" className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/564x/62/ac/c3/62acc3f90f8f0788aab8ecb2fded2558.jpg" />
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-            <Row gutter={[20, 48]} align="center">
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 1" className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/originals/05/f7/ea/05f7ea4755741c023f9e75e3df4b8212.jpg"/>
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 2"  className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/564x/c2/4c/d9/c24cd920faacaa8dbaa2cbbafdf83ac9.jpg" />
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 3" className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/564x/b3/42/d6/b342d651598fde2d9cfa8276c0a68df9.jpg" />
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="gutter-row" span={4}>
-                    <img ant="movie 4" className="nowShowingMovie" alt="movie" src="https://i.pinimg.com/564x/62/ac/c3/62acc3f90f8f0788aab8ecb2fded2558.jpg" />
-                    <Row>
-                        <Col>
-                        <h3 className="movieTitle">movieTitle</h3>
-                        </Col>
-                    </Row>
-                    
-                    <Row>
-                        <Col> 
-                        <h4 id="genre">Genre</h4>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-           
-            </div>
-            
-        
+        <Row gutter={[20, 48]} align="center">
+            <Col className="gutter-row" span={4}>
+                <img ant={props.key} className="nowShowingMovie" alt="movie" src={props.movie.movie_img}/>
+                <Row>
+                    <Col>
+                    <h3 className="movieTitle">{props.movie.movie_name}</h3>
+                    </Col>
+                </Row>
+                
+                <Row>
+                    <Col> 
+                    <h4 id="genre">Genre</h4>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
       </div>
     );
   }
-  export default NowShowingCarousel;
+  export default NowShowing;
---
+++ src/features/chingqueue/components/NowShowingList.js
@@ -0,0 +1,41 @@
+
+import { Divider } from 'antd';
+import '../styles/nowShowing.css'
+import { getAllMovies } from '../../apis/MovieApi'
+import { useSelector, useDispatch} from 'react-redux';
+import { AddAllMoviesToState, selectAllMovies } from './reducers/MovieReducer'
+import NowShowing from './NowShowing'
+
+function NowShowingList(){
+    const dispatch = useDispatch()
+    getAllMovies().then((response) => {
+        const movieList = response.data.map((movie) => 
+            (
+                {
+                    id: movie.movie_id, 
+                    movie_name: movie.movie_name,
+                    movie_img: movie.movie_img,
+                    genre: movie.genre
+                }
+            )
+        )
+        dispatch(AddAllMoviesToState(movieList))
+    })
+
+    const movies = useSelector(selectAllMovies)
+
+    return(
+      <div>
+            <div>
+            <Divider orientation="left"></Divider>
+                <h2 className="nowShowing">Now Showing</h2>
+                {
+                    movies.map((movie) => (
+                        <NowShowing key={movie.id} movie={movie}/>
+                    ))   
+                }
+            </div>
+      </div>
+    );
+  }
+  export default NowShowingList;
