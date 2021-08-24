ba2bce21f524ac72fedfd37fd10121719b0e8f5f
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-24 16:36:11 +0800

    [ST103][Robert] refact: Removed unnecessary routing for login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './features/chingqueue/styles/App.css'
-import UserLogin from './features/chingqueue/components/UserLogin'
 import {Route, BrowserRouter, Switch } from 'react-router-dom';
 import Index from './features/chingqueue/components/Index';
 import 'antd/dist/antd.css';
@@ -10,7 +9,6 @@ function App() {
        <BrowserRouter>
        <Switch>
            <Route exact path="/" component={Index}/>
-           <Route exact path="/login" component={UserLogin}/>
         </Switch>
       </BrowserRouter>
     </div>
