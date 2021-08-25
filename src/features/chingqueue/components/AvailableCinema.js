6451c2703d79bcbe6a9d3308dd0b6f85b28cf986
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-25 22:20:20 +0800

    Merged displaying of cinema location with some bug fixes

--- src/features/chingqueue/components/AvailableCinema.js
+++ src/features/chingqueue/components/AvailableCinema.js
@@ -2,29 +2,27 @@ import { List, Divider, Typography, Button } from "antd";
 import React from "react";
 import '../styles/availableCinema.css';
 
-function AvailableCinema(){
+function AvailableCinema(props){
     const data = [
-        <span className="list-button">
-            <Button className="button-time-slot">10:00 AM</Button>
-            <Button className="button-time-slot">12:30 PM</Button>
-            <Button className="button-time-slot">2:00 PM</Button>
-        </span>,
-        <span className="list-button">
-            <Button className="button-time-slot">10:00 AM</Button>
-            <Button className="button-time-slot">12:30 PM</Button>
-            <Button className="button-time-slot">2:00 PM</Button>
-        </span>,
+        <span className="list-button">         
+        {
+            props.cinema.cinema_timeslot_list.map((timeslot)=>(
+                <Button className="button-time-slot">{timeslot}</Button>
+            ))
+        }
+        </span>
       ];
+
     return(
         <React.Fragment>
             <div className="available-cinema">
-            <Divider orientation="left"> Available Cinema's </Divider>
+           {/* { <Divider orientation="left"> Available Cinemas </Divider>} */}
             <List
                 bordered
-                dataSource={data}
+                 dataSource={data}
                 renderItem={item => (
                     <List.Item>
-                    <Typography.Text className="cinema-name-location" mark>[Cinema name: Location]</Typography.Text> {item}
+                    <Typography.Text className="cinema-name-location" mark>{props.cinema.cinema_company} : {props.cinema.cinema_location}</Typography.Text> {item}
                     </List.Item>
                 )}
             />
--- src/features/store/store.js
+++ src/features/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from '../chingqueue/components/reducers/UserReducer';
 import movieReducer from '../chingqueue/components/reducers/MovieReducer';
+import CinemaReducer from "../chingqueue/components/reducers/CinemaReducer";
 import reviewReducer from '../chingqueue/components/reducers/MovieReviewsReducer'
 
 const store = configureStore({
-    reducer: {currentUser: userReducer, movieList: movieReducer, reviewList: reviewReducer}
+    reducer: {currentUser: userReducer, movieList: movieReducer, reviewList: reviewReducer, cinemas: CinemaReducer}
 });
 
 export default store;
