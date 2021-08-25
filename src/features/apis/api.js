dc7d7c70715ac0a39c0f68fe5af47b0ff2132f37
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-25 15:24:11 +0800

    [ChingQueue][Cedie]docu: added url for both local and heroku url

--- src/features/apis/api.js
+++ src/features/apis/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: 'http://localhost:8080'
-    baseURL: 'https://chingqueue-staging-backend.herokuapp.com'
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080'
 })
 
 export default api;
