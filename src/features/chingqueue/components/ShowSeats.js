66044c89f3524ed70865f91bffe2c5a64475f23c
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-26 14:57:05 +0800

    [ST304_Choose_Available_Reservation_Slots][Kyle] feat: Available reservation slots

--- src/features/chingqueue/components/ShowSeats.js
+++ src/features/chingqueue/components/ShowSeats.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Col, Popconfirm, Row, message, Modal } from 'antd'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllCinemaSeatsByCinemaId } from '../../apis/CinemaSeatsApi'
 import { AddAllCinemaSeats, selectAllCinemaSeatIds } from '../components/reducers/CinemaSeatReducer'
 import Seat from './Seat'
+import "./../styles/CinemaSeats.css"
 
 function ShowSeats(props) {
     const dispatch = useDispatch()
+    const [currSeat, setSeat] = useState(false);
+    const text = "Are you sure you want this seat?";
     
     getAllCinemaSeatsByCinemaId(props.cinemaId).then((response) => {
         let cinemaSeats = response.data.map((cinemaSeats) => 
@@ -19,16 +23,56 @@ function ShowSeats(props) {
         dispatch(AddAllCinemaSeats(cinemaSeats))
     })
 
+    const [isModalVisible] = useState(false);
+    let mySeat = currSeat ? "selected" : "";
+    let ticketPrice = 240;
+    let bookFee = 49;
+    let total = 0;
+    let selectedSeat = 0;
+    if (mySeat === "selected") {
+        selectedSeat = selectedSeat + 1;
+        total = ticketPrice + total + bookFee;
+    }
+    
+    function confirm() {
+        
+        success();
+      }
+
+      function success() {
+        Modal.success({
+          title: 'QR Code payment',
+          content: (
+            <div>
+              <img id='barcode' src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100" alt="" title="HELLO" width="250" height="250" />
+            </div>
+          ),
+          onOk() {
+            message.info('You have successfully reserved this seat');
+          },
+        });
+      }
+      
     const seats = useSelector(selectAllCinemaSeatIds)
     console.log(seats)
     return (
-        <div>
+        <div className="seatssContainer">
+            <Modal title="QR Payment" visible={isModalVisible}>
+            </Modal>
+          
+            <Row>
             {
                 seats.map((id) => 
                 (
-                    <Seat key={id} seatId={id}/>
+                <Popconfirm placement="top" onConfirm={confirm} title={text}> <Col> <div className={`seat ${mySeat}`} onClick={() => setSeat('selected')}><Seat key={id} seatId={id}/> </div></Col> </Popconfirm>
                 )
                 )}
+            </Row>
+            <Row className="paid"><span className="paidFont">Booking Summary: </span></Row>
+            <Row className="paid"><span className="paidFont">Selected Seats: {selectedSeat}</span> </Row>
+            <Row><span className="paidFont">Reservation Fee: {bookFee}</span></Row>
+            <Row><span className="paidFont">Total Amount: {total}</span></Row>
+           
         </div>
     )
 }
