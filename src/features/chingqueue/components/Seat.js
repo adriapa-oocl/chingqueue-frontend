c8251495cc1f42f008e492a259dc1395a379c71f
deverca <deverca@oocl.com>
2021-08-26 14:55:26 +0800

    [ST304][Carmela]feat:  added choose reservation slot logic

--- src/features/chingqueue/components/Seat.js
+++ src/features/chingqueue/components/Seat.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectCinemaSeatById, UpdateCinemaSeat } from '../components/reducers/CinemaSeatReducer'
-
+import {message, Popconfirm} from 'antd';
+import {updateCinemaSeat} from '../../apis/CinemaSeatsApi'
 function Seat(props) {
     const dispatch = useDispatch();
     const seat = useSelector((state) => selectCinemaSeatById(state, props.seatId))
 
     function handleClick() {
         const seatUpdate = {
-            id: seat.id, 
-            seat_id: seat.seat_id, 
-            cinema_id: seat.cinema_id, 
+            seatId: seat.seat_id, 
+            cinemaId: seat.cinema_id, 
             availability: !seat.availability
         }
-        // console.log(seatUpdate)
-        dispatch(UpdateCinemaSeat(seatUpdate))
+
+        console.log(seatUpdate.seatId);
+
+        if(seat.availability===true){
+        updateCinemaSeat(seat.seat_id, {updateInfo:seatUpdate}).then(response =>{
+                   dispatch(UpdateCinemaSeat({id:seat.id, updateSeat:response.data}))   
+                   console.log(response.data);         
+        })
+        }else{
+            console.log("Seat taken");
+        }
+
+ 
     }
+
 
     return (
         <div>
-            <span onClick={handleClick}>
-                {seat.id}
+            <span>
+                <Popconfirm 
+                title="Are you sure to reserve this seat?"
+                onConfirm = {handleClick}
+                okText="Yes"
+                cancelText="No">
+                        {seat.id}
+                
+                </Popconfirm>
+        
             </span>
         </div>
 
